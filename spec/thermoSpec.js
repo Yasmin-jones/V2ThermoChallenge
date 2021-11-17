48a751afc7ad661bed23e588591310995671763f
Mia Gordon <mia.gordon@digital.justice.gov.uk>
2021-11-17 15:51:48 +0000

    First user story passes: starting temp

--- spec/thermoSpec.js
+++ spec/thermoSpec.js
@@ -1,46 +1,15 @@
 describe("Thermostat", function() {
 
-  beforeEach(function(){
+  'use strict';
+
+  let thermostat;
+
+  beforeEach(() => {
     thermostat = new Thermostat();
   });
 
-  describe('Thermostat starts at 20 degrees', function(){
-    it('starts at 20deg', function(){
-      expect(thermostat.temperature).toEqual(20);
-    });
+  it('thermostat starts at 20 degrees', () => {
+    expect(thermostat.startTemp).toEqual(20);
   });
-
-  describe('Can increase the temperature', function(){
-    it('increase temperature', function(){
-      expect(thermostat.increase_temperature(5)).toEqual(25);
-    });
-  });
-
-  describe('Can increase the temperature', function(){
-    it('increase temperature', function(){
-      thermostat.increase_temperature(5);
-      expect(thermostat.increase_temperature(5)).toEqual(30);
-    });
-  });
-
-  describe('Can decrease the temperature', function(){
-    it('decreases temperature', function(){
-      expect(thermostat.decrease_temperature(2)).toEqual(18); 
-    });
-  });
-
-  describe('The minimum temperature is 10', function() {
-    it('Has a min temperature of 10', function(){
-      expect(thermostat.MIN_TEMP).toEqual(10); 
-    })
-
-    it('has min temp of 10', function(){
-      for (let i = 0; i < 11; i++) {
-        thermostat.decrease_temperature(6);
-      }
-      expect(thermostat.temperature).toEqual(10); 
-    })
-  })
-
 
 });
--- src/thermo.js
+++ src/thermo.js
@@ -1,39 +1,13 @@
-class Thermostat {
-  
-  constructor(temperature) { 
-  this.temperature = 20;
-  this.MIN_TEMP = 10;
-  } 
-  
-  temperature() {
-    return this.temperature ;
+'use strict';
+
+class Thermostat{
+
+  constructor() {
+    this.startTemp = 20;
   }
 
-  increase_temperature(number){
-   this.temperature += number;
-    return this.temperature;
+  startTemp() {
+    return this.startTemp;
   }
 
-  decrease_temperature(number) {
-    if (this.temperature > this.MIN_TEMP){
-      this.temperature -= number; 
-      return this.temperature 
-    }
-    else if (this.temperature === this.MIN_TEMP) {
-        return this.MIN_TEMP
-    }
-    else {
-      return this.MIN_TEMP;
-    }
-    
-  }
-
-}
-let thermo = new Thermostat();
-// console.log(thermo.temperature);
-// console.log(thermo.increase_temperature(5));
-// console.log(thermo.increase_temperature(5));
-
-// console.log(thermo.decrease_temperature(5));
-// console.log(thermo.decrease_temperature(5));
-// console.log(thermo.increase_temperature(5));
+};
